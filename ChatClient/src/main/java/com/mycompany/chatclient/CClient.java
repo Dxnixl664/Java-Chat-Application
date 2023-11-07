7ce667abcc69e8d9766ab8c5211489dba843fcff
Dxnixl <dcg.sspm@gmail.com>
2023-11-07 04:13:51 -0800

    Client now handles incoming messages in real-time

--- ChatClient/src/main/java/com/mycompany/chatclient/CClient.java
+++ ChatClient/src/main/java/com/mycompany/chatclient/CClient.java
@@ -7,33 +7,48 @@ public class CClient {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private Thread listenerThread;
+    private boolean stopFlag = false;
     
     public void startConnection(String ip, int port) {
         try {
             clientSocket = new Socket(ip, port);
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+            // Starting separate thread to listen for messages from server
+            listenerThread = new Thread(() -> {
+                try {
+                    String fromServer;
+                    while (!stopFlag && (fromServer = in.readLine()) != null) {
+                        System.out.println("Server: " + fromServer);
+                    }
+                } catch (IOException e) {
+                    if (!stopFlag) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            listenerThread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
-    public String sendMessage(String msg) {
-        try {
-            out.println(msg);
-            return in.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public void sendMessage(String msg) {
+        out.println(msg);
     }
     
     public void stopConnection() {
+        stopFlag = true;
         try {
+            if (listenerThread != null) {
+                listenerThread.join(); // Wait for listener thread to finish
+            }
             in.close();
             out.close();
             clientSocket.close();
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
--- ChatClient/src/main/java/com/mycompany/chatclient/ChatClient.java
+++ ChatClient/src/main/java/com/mycompany/chatclient/ChatClient.java
@@ -5,8 +5,7 @@ public class ChatClient {
     public static void main(String[] args) {
         CClient client = new CClient();
         client.startConnection("127.0.0.1", 6666);
-        String response = client.sendMessage("hello server");
-        System.out.println(response);
+        client.sendMessage("Hello from the client!");
         client.stopConnection();
     }
 }
